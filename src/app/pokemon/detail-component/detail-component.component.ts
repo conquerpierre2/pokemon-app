999f21e4ab6c36fa820a8ef21136d3b98f44eb99
Sinegre <pierre.sinegre@etu.univ-rouen.fr>
2022-09-23 14:18:12 +0200

    Initailised project pokemon

---
+++ src/app/pokemon/detail-component/detail-component.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
+
+@Component({
+  selector: 'app-detail-component',
+  templateUrl: './detail-component.component.html',
+  styles: [
+  ]
+})
+export class DetailComponentComponent implements OnInit {
+
+  pokemonList: Pokemon[];
+  pokemon : Pokemon | undefined;
+
+  constructor(private route: ActivatedRoute, private router: Router, private pokemonService : PokemonService) {
+
+    
+   }
+
+  ngOnInit(): void {
+    const pokemonId: string | null = this.route.snapshot.paramMap.get('id');
+    if(pokemonId) {
+      this.pokemonService.getPokemonById(+pokemonId).subscribe(pokemon =>  this.pokemon = pokemon);
+
+    } 
+    
+  }
+
+  deletePokemon(pokemon: Pokemon) {
+    this.pokemonService.deletePokemonById(pokemon.id).subscribe(
+      () => this.goToPokemonList()
+    )
+  }
+
+  goToPokemonList() : void {
+        this.router.navigate(['/pokemons'])
+  }
+
+  goToEditPokemon(pokemon : Pokemon ) {
+    this.router.navigate(['/edit/pokemon', pokemon.id])
+  }
+
+}
---
+++ src/app/pokemon/pokemon.service.ts
@@ -0,0 +1,99 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, of, tap } from 'rxjs';
+import { Pokemon } from './pokemon';
+
+@Injectable()
+export class PokemonService {
+
+  constructor(private http: HttpClient) {
+    
+  }
+
+  getPokemonList(): Observable<Pokemon[]> {
+    return this.http.get<Pokemon[]>('api/pokemons').pipe(
+      tap( (response) => this.log(response)),
+      catchError( error => this.handleError(error,[]))
+      
+
+    );
+
+  }
+
+  getPokemonById(id : number): Observable<Pokemon | undefined> {
+        return this.http.get<Pokemon>(`api/pokemons/${id}`).pipe(
+          tap((response) => this.log(response)),
+          catchError( error => this.handleError(error, undefined))
+
+        );
+  }
+
+  updatePokemon(pokemon : Pokemon ) : Observable<null> {
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type' : 'application/json'})
+    }
+
+    return this.http.put('api/pokemons/' , pokemon, httpOptions).pipe(
+      tap(response => this.log(response)),
+      catchError(error => this.handleError(error, null))
+    );
+
+  }
+
+  deletePokemonById(pokemonId: number): Observable<null> {
+
+    return this.http.delete(`api/pokemons/${pokemonId}`).pipe(
+      tap(response => this.log(response)),
+      catchError(error => this.handleError(error, null))
+
+    );
+
+  }
+
+  addPokemon(pokemon : Pokemon) : Observable<Pokemon> {
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type' : 'application/json'})
+    };
+
+    return this.http.post<Pokemon>('api/pokemons', pokemon, httpOptions).pipe(
+      tap(response => this.log(response)),
+      catchError(error => this.handleError(error, null))
+
+    );
+
+
+    
+
+  }
+
+
+  searchPokemonList(term: string) : Observable<Pokemon[]> {
+    if(term.length <= 1) {
+      return of([]);
+    }
+    return this.http.get<Pokemon[]>(`api/pokemons/?name=${term}`).pipe(
+      tap(response => this.log(response)),
+      catchError(error => this.handleError(error, []))
+
+    )
+
+  }
+
+
+  private log(response: any) {
+    console.table(response);
+
+
+  }
+
+  private handleError(error : Error, errorValue : any) {
+
+    console.log(error);
+    return of(errorValue);
+
+  }
+
+  getPokemonTypeList() : string[] {
+    return ['Plante', 'Feu', 'Eau', 'Insecte', 'Normal', 'Electrik', 'Poison', 'Fée', 'Vol', 'Combat', 'Psy'];
+  }
+}
